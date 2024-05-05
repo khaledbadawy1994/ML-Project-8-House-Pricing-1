e4ec732229fb5a7ac5699d111a9a834b8dc4274c
khaledbadawy1994 <166828748+khaledbadawy1994@users.noreply.github.com>
2024-05-05 13:11:06 -0700

    Update House_Pricing.py

--- House_Pricing.py
+++ House_Pricing.py
@@ -1481,7 +1481,6 @@ fig.update_layout(paper_bgcolor="#42EADD", plot_bgcolor='#42EADD')
 fig.show()
 
 missing_train= remove_outliers_zscore(missing_train,['GrLivArea'])
-Total number of outliers detected and Deleted are : 5
 
 #Plot Box Plot to Seee Outliers in LotArea in Plotly
 fig = px.box(missing_train, y="LotFrontage")
